960fdf4e3f87f896e416cf7a8e5900d1d0f8d45d
Kellen-N <kellenswang3@gmail.com>
2025-12-23 15:34:59 -0600

    Grappling Bug Fixed
    The grappling ability still needs work.

--- PlayerController/States/Abilities/grappling.gd
+++ PlayerController/States/Abilities/grappling.gd
@@ -7,6 +7,7 @@ extends State
 @export var diving_state : State
 @export var jumping_state : State
 @export var walking_state : State
+@export var ascending_state : State
 @export_category("Parameters")
 @export var grapple_duration : float #seconds
 @export var grappling_speed: float
@@ -18,12 +19,20 @@ extends State
 @export var ray_left: RayCast2D
 @export var ray_right: RayCast2D
 
+@export var line_up: Line2D
+@export var line_down: Line2D
+@export var line_left: Line2D
+@export var line_right: Line2D
+
+@onready var rays : Array = [ray_left, ray_right, ray_up, ray_down]
+
 var gravity : int
 var max_falling_speed : int
 var horizontal_input : int = 0
 var direction : String
 var jump_input_buffer: Timer
 var grapple_timer: Timer
+var no_collision = false
 
 func _ready() -> void:
 	#Input buffer setup:
@@ -33,7 +42,7 @@ func _ready() -> void:
 	add_child(jump_input_buffer)
 	
 	grapple_timer = Timer.new()
-	grapple_timer.wait_time = grappling_speed
+	grapple_timer.wait_time = grapple_duration
 	grapple_timer.one_shot = true
 	add_child(grapple_timer)
 
@@ -42,8 +51,23 @@ func set_direction(ability_direction : String) -> void:
 
 func activate(last_state : State) -> void:
 	super(last_state) #Call activate as defined in state.gd and then also do:
-	
+	grapple_timer.start()
 	parent.velocity = Vector2(0,0)
+	await grapple_timer.timeout
+	#for ray in rays: 
+		#if ray.is_colliding:
+			#match direction:
+				#"left":
+					#parent.velocity.x = -grappling_speed
+				#"right":
+					#parent.velocity.x = grappling_speed
+				#"up":
+					#parent.velocity.y = -grappling_speed
+				#"down":
+					#parent.velocity.y = grappling_speed
+				#_:
+					#pass
+					
 	if direction == "up" and ray_up.is_colliding():
 		print("up")
 		parent.velocity.y = -grappling_speed
@@ -56,15 +80,21 @@ func activate(last_state : State) -> void:
 	elif direction == "right" and ray_right.is_colliding():
 		print("right")
 		parent.velocity.x = grappling_speed
+	else:
+		no_collision = true
 
 func process_input(event : InputEvent) -> State:
+	if event.is_action_pressed("jump"):
+		jump_input_buffer.start()
 	if event.is_action_pressed("dive"):
 		return diving_state
 	return null
 
 func process_physics(_delta) -> State:
 	parent.move_and_slide()
-	if parent.is_on_wall() or parent.is_on_ceiling():
+	if (parent.is_on_wall() and parent.velocity == Vector2(0,0)) or parent.is_on_ceiling():
+		return falling_state
+	if (parent.velocity == Vector2(0,0) and grapple_timer.time_left == 0):
 		return falling_state
 	if (parent.is_on_floor()):
 		if jump_input_buffer.time_left > 0:
