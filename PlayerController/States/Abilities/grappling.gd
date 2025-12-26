c5c428a0f563e4592a0b78fe9bd38024fff3a06b
Kellen-N <kellenswang3@gmail.com>
2025-12-26 13:06:59 -0600

    Grappling Ability Fully Created
    There are still some bugs and parameters to work out but it is mostly functional.

--- PlayerController/States/Abilities/grappling.gd
+++ PlayerController/States/Abilities/grappling.gd
@@ -11,10 +11,11 @@ extends State
 @export_category("Parameters")
 @export var grapple_duration : float #seconds
 @export var grappling_speed: float
-@export var jump_input_buffer_patience : float
-@export_category("References")
-
 @export var grapple_max_length: float
+@export var grapple_increment: float
+@export var jump_input_buffer_patience : float
+
+@export_category("References")
 @export var grapple_ray: RayCast2D
 @export var line: Line2D
 
@@ -26,6 +27,7 @@ var jump_input_buffer: Timer
 var grapple_timer: Timer
 var grapple_current_length: float
 var has_collided = false
+enum grappling_speeds {SLOW = 500, MODERATE = 2000, FAST = 3000, EXTREME = 4000}
 
 func _ready() -> void:
 	#Input buffer setup:
@@ -46,7 +48,6 @@ func activate(last_state : State) -> void:
 	super(last_state) #Call activate as defined in state.gd and then also do:
 	has_collided = false
 	grapple_current_length = 0
-	line.clear_points()
 	line.add_point(Vector2.ZERO)
 	line.add_point(Vector2.ZERO)
 	parent.velocity = Vector2(0,0)
@@ -62,10 +63,12 @@ func process_input(event : InputEvent) -> State:
 func process_physics(delta) -> State:
 	parent.move_and_slide()
 	if !has_collided and grapple_current_length < grapple_max_length:
-		grapple_current_length += 1500 * delta
+		grapple_current_length += grapple_increment * delta
 		if grapple_current_length > grapple_max_length:
 			grapple_current_length = grapple_max_length
+			
 		print(grapple_current_length)
+		
 		match direction:
 			"right":
 				grapple_ray.target_position = Vector2(grapple_current_length,0)
@@ -81,6 +84,15 @@ func process_physics(delta) -> State:
 				line.set_point_position(1, Vector2(0, grapple_current_length))
 		
 		if grapple_ray.is_colliding():
+			if grapple_current_length > grapple_max_length * .75:
+				grappling_speed = grappling_speeds.EXTREME
+			elif grapple_current_length > grapple_max_length * .50:
+				grappling_speed = grappling_speeds.FAST
+			elif grapple_current_length > grapple_max_length * .25:
+				grappling_speed = grappling_speeds.MODERATE
+			elif grapple_current_length < grapple_max_length * .25:
+				grappling_speed = grappling_speeds.SLOW
+			
 			match direction:
 				"right":
 					parent.velocity.x = grappling_speed
@@ -91,18 +103,23 @@ func process_physics(delta) -> State:
 				"down":
 					parent.velocity.y = grappling_speed
 			has_collided = true
+			line.clear_points()
 
 	
 	if (parent.is_on_wall() and parent.velocity == Vector2(0,0)) or parent.is_on_ceiling():
 		return falling_state
-	if (parent.velocity == Vector2(0,0) and has_collided):
+	elif (!has_collided and grapple_current_length == grapple_max_length):
 		return falling_state
+	elif (has_collided and parent.velocity.x == 0):
+		return falling_state
+	
 	if (parent.is_on_floor()):
 		if jump_input_buffer.time_left > 0:
 			return jumping_state
-		else:
+		elif has_collided and parent.velocity == Vector2(0,0):
 			return idle_state
 	return null
 
 func deactivate(_next_state) -> void:
 	line.clear_points()
+	grapple_ray.target_position = Vector2(0,0)
