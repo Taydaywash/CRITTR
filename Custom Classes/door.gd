aec34937af3aefc7cb98a33d084c1d13e09b678a
Taydaywash <tayvenwashington1@gmail.com>
2026-02-06 08:46:45 -0600

    Added fade out when transitioning doors

--- Custom Classes/door.gd
+++ Custom Classes/door.gd
@@ -2,8 +2,21 @@ class_name door
 extends interactable
 
 @export var go_to_door : Node2D
+var fade_player_out : bool = false
 
 func _input(_event: InputEvent) -> void:
 	if overlapping and can_interact():
 		if Input.is_action_just_pressed("interact"):
+			door_transition()
+
+func door_transition() -> void:
+	fade_player_out = true
+
+func _process(delta: float) -> void:
+	super(delta)
+	if fade_player_out:
+		player.modulate = lerp(player.modulate,Color(0.0,0.0,0.0,0.0), clampf(delta * 10,0.0,1.0))
+		if player.modulate.a <= 0.1:
+			fade_player_out = false
+			player.modulate = Color(1.0,1.0,1.0,1.0)
 			player.global_position = Vector2(go_to_door.global_position.x,go_to_door.global_position.y + 20)
--- PlayerController/camera_controller.gd
+++ PlayerController/camera_controller.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
 @export var default_zoom : float
+var limit_smoothed_timer
 
 func _ready() -> void:
 	change_zoom_to(default_zoom)
@@ -11,6 +12,7 @@ func change_bounds_to(bounds : Dictionary):
 	limit_bottom = bounds.bottom
 	limit_left = bounds.left
 	limit_right = bounds.right
+	reset_smoothing()
 	position_smoothing_enabled = true
 	
 func change_zoom_to(zoom_in):
