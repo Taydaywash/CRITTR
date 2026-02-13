73fe0dfe1e6740246a2cc1aa6c730e8e0ce5e771
Taydaywash <tayvenwashington1@gmail.com>
2026-02-13 15:10:39 -0600

    Added option menu functionality

--- Title/title_screen.gd
+++ Title/title_screen.gd
@@ -1,7 +1,12 @@
 extends Control
 
+const WORLD = preload("res://World/World.tscn")
+
+func _ready() -> void:
+	get_tree().paused = false
+
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://World/World.tscn")
+	get_tree().change_scene_to_packed(WORLD)
 
 func _on_setting_button_pressed() -> void:
 	pass # Replace with function body.
--- UI/Pause/options_tab.gd
+++ UI/Pause/options_tab.gd
@@ -1,6 +1,50 @@
 extends pause_tab
 
 @onready var window = get_window()
+const TITLE_SCREEN = "res://Title/TitleScreen.tscn"
+
+@export var screen_size_dropdown : OptionButton
+@export var resolution_dropdown : OptionButton
+
+var options : Dictionary = {
+	"screen_size": DisplayServer.WINDOW_MODE_FULLSCREEN,
+	"borderless" : false,
+	"screen_resolution" : Vector2(1152, 648),
+	"text_speed" : 0,
+}
+
+func _ready() -> void:
+	update_screen_settings()
+
+func update_screen_settings():
+	DisplayServer.window_set_mode(options.screen_size)
+	if (options.screen_size != DisplayServer.WINDOW_MODE_FULLSCREEN):
+		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		window.size = options.screen_resolution
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, options.borderless)
+	screen_size_dropdown.selected = get_screen_size_dropdown_index()
+	resolution_dropdown.selected = get_resolution_dropdown_index()
+	if (options.screen_size == DisplayServer.WINDOW_MODE_FULLSCREEN and 
+	DisplayServer.window_get_flag(DisplayServer.WINDOW_FLAG_BORDERLESS)):
+		print("f")
+		#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+func get_screen_size_dropdown_index() -> int:
+	var index = 0
+	match options.screen_size:
+		DisplayServer.WINDOW_MODE_FULLSCREEN: index =  0
+		DisplayServer.WINDOW_MODE_WINDOWED: index = 2
+	index += int(options.borderless)
+	return index
+func get_resolution_dropdown_index():
+	var index = 0
+	match options.screen_resolution:
+		Vector2(3840, 2160): index = 0
+		Vector2(1920, 1080): index = 1
+		Vector2(1440, 900): index = 2
+		Vector2(1366, 768): index = 3
+		Vector2(1280, 720): index = 4
+		Vector2(1152, 648): index = 5
+	return index
 
 func handle_input(event: InputEvent) -> void:
 	if event.is_action_released("ui_cancel"):
@@ -12,29 +56,29 @@ func handle_input(event: InputEvent) -> void:
 
 func _on_screen_size_item_selected(index: int) -> void:
 	match index:
-		0: 
-			#window.mode = Window.MODE_FULLSCREEN
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
-		1: 
-			#window.mode = Window.FLAG_BORDERLESS
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
-		2: 
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		0: #Fullscreen
+			options.screen_size = DisplayServer.WINDOW_MODE_FULLSCREEN
+			options.borderless = false
+		1: #Borderless fullscreen
+			options.screen_size = DisplayServer.WINDOW_MODE_FULLSCREEN
+			options.borderless = true
+		2: #Windowed
+			options.screen_size = DisplayServer.WINDOW_MODE_WINDOWED
+			options.borderless = false
+		3: #Borderless Windowed
+			options.screen_size = DisplayServer.WINDOW_MODE_WINDOWED
+			options.borderless = true
+	update_screen_settings()
 
 func _on_resolution_item_selected(index: int) -> void:
 	match index:
-		0: window.size = Vector2(3840, 2160) #print("3840 x 2160")
-		1: window.size = Vector2(1920, 1080) #print("1920 x 1080")
-		2: window.size = Vector2(1440, 900) #print("1440 x 900")
-		3: window.size = Vector2(1366, 768) #print("1366 x 768")
-		4: window.size = Vector2(1280, 720) #print("1280 x 720")
-		5: window.size = Vector2(1152, 684) #print("1152 x 684")
-	window.content_scale_size = window.size
-
-func _on_setting_button_pressed() -> void:
-	pass # Replace with function body.
+		0: options.screen_resolution = Vector2(3840, 2160) #print("3840 x 2160")
+		1: options.screen_resolution = Vector2(1920, 1080) #print("1920 x 1080")
+		2: options.screen_resolution = Vector2(1440, 900) #print("1440 x 900")
+		3: options.screen_resolution = Vector2(1366, 768) #print("1366 x 768")
+		4: options.screen_resolution = Vector2(1280, 720) #print("1280 x 720")
+		5: options.screen_resolution = Vector2(1152, 648) #print("1152 x 648")
+	update_screen_settings()
+	
 func _on_main_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://Title/TitleScreen.tscn")
+	get_tree().change_scene_to_file(TITLE_SCREEN)
