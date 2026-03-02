ce0c591917c1f4e8683968d2571e8796ddcea14f
Taydaywash <tayvenwashington1@gmail.com>
2026-03-02 08:17:13 -0600

    added minimum speed for dash_jump particles

--- Scenes/Particle Controller/Particles/Player/dash_jump_ring.gd
+++ Scenes/Particle Controller/Particles/Player/dash_jump_ring.gd
@@ -1,8 +1,10 @@
 extends Particle
 
+@export var player_min_velocity : int
+
 func _process(_delta: float) -> void:
 	if player:
-		if player.velocity.y == 0 or  player.velocity.x == 0:
-			return
+		if player.velocity.y == 0 or  player.velocity.x == 0 or player.velocity.length() < player_min_velocity:
+			emitting = false
 		angle_min = rad_to_deg(get_angle_to(global_position + player.velocity.normalized()))
 		angle_max = rad_to_deg(get_angle_to(global_position + player.velocity.normalized()))
