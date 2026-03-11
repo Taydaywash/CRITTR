3193d95ef8b691f5a39756585a4d9ca5c044ae85
Kellen-N <kellenswang3@gmail.com>
2026-03-11 14:04:16 -0500

    Fixed Lost Changes

--- Scenes/World/LevelEditor/Objects/Steam/steam.gd
+++ Scenes/World/LevelEditor/Objects/Steam/steam.gd
@@ -4,8 +4,8 @@ extends Area2D
 @export var x: int
 @export var y: int
 @export_category("Timer")
-@export var active_time: float = 1
-@export var inactive_time: float = 1
+@export var active_time: float = 2
+@export var inactive_time: float = 2
 @export var delay_time: float
 
 @onready var collision: CollisionShape2D = $CollisionShape2D
@@ -16,15 +16,16 @@ extends Area2D
 func _ready():
 	active_timer.wait_time = active_time
 	inactive_timer.wait_time = inactive_time
-	collision.size = Vector2(x, y)
+	collision.shape.size.x = x
+	collision.shape.size.y = y
 	active_timer.start()
 
 func _on_active_timer_timeout():
 	inactive_timer.start()
-	self.monitoring = false
+	self.monitorable = false
 	self.visible = false
 
 func _on_inactive_timer_timeout():
 	active_timer.start()
-	self.monitoring = true
+	self.monitorable = true
 	self.visible = true
