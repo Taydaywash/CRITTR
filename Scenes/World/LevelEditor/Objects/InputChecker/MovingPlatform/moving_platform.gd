48567e80490605324df9fc1ddf0f8d8036308d61
Kellen-N <kellenswang3@gmail.com>
2026-04-25 15:53:40 -0500

    Placeholder Levels For Temple

--- Scenes/World/LevelEditor/Objects/InputChecker/MovingPlatform/moving_platform.gd
+++ Scenes/World/LevelEditor/Objects/InputChecker/MovingPlatform/moving_platform.gd
@@ -4,6 +4,7 @@ extends Path2D
 @onready var path = $PathFollow2D
 var moving_forward = true
 @export var duration: float = 5
+@export var timer_offset: float = 0
 
 func _ready():
 	get_parent().connect("sequence_matched", start_timer)
@@ -13,8 +14,8 @@ func _ready():
 func start_timer() -> void:
 	timer.start()
 	var tween = create_tween()
-	tween.tween_property(path, "progress_ratio", 1.0, 1.0).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(path, "progress_ratio", 1.0, 1.0 + timer_offset).set_trans(Tween.TRANS_SINE)
 
 func _on_timer_timeout() -> void:
 	var tween = create_tween()
-	tween.tween_property(path, "progress_ratio", 0.0, 1.0).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(path, "progress_ratio", 0.0, 1.0 + timer_offset).set_trans(Tween.TRANS_SINE)
