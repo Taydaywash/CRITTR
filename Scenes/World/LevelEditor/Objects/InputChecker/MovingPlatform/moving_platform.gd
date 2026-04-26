d4119160bc31c30acb956f1f8e984c2f9504bbd4
Kellen-N <kellenswang3@gmail.com>
2026-04-26 00:18:36 -0500

    More MORE LEVELS

--- Scenes/World/LevelEditor/Objects/InputChecker/MovingPlatform/moving_platform.gd
+++ Scenes/World/LevelEditor/Objects/InputChecker/MovingPlatform/moving_platform.gd
@@ -1,23 +1,32 @@
-
 extends Path2D
 @onready var timer = $Timer
 @onready var path = $PathFollow2D
 var moving_forward = true
+var tween: Tween
 @export var duration: float = 5
 @export var timer_offset: float = 0
 @export var stay: bool = false
 
 func _ready():
 	get_parent().connect("sequence_matched", start_timer)
+	EventController.connect("player_respawn", player_death)
 	timer.wait_time = duration
 	path.progress_ratio = 0.0
 
 func start_timer() -> void:
 	timer.start()
-	var tween = create_tween()
+	if tween:
+		tween.kill()
+	tween = create_tween()
 	tween.tween_property(path, "progress_ratio", 1.0, 1.0 + timer_offset).set_trans(Tween.TRANS_SINE)
 
 func _on_timer_timeout() -> void:
 	if (!stay): 
-		var tween = create_tween()
+		tween = create_tween()
 		tween.tween_property(path, "progress_ratio", 0.0, 1.0 + timer_offset).set_trans(Tween.TRANS_SINE)
+
+func player_death() -> void:
+	if tween:
+		tween.kill()
+	path.progress_ratio = 0
+	path.progress = 0
