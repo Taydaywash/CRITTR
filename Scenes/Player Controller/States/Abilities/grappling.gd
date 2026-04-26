3eeccd85dd5ba8d240be227e2fae3927629a1ca2
Taydaywash <tayvenwashington1@gmail.com>
2026-04-26 12:50:30 -0500

    added grappling sfx

--- Scenes/Player Controller/States/Abilities/grappling.gd
+++ Scenes/Player Controller/States/Abilities/grappling.gd
@@ -45,12 +45,17 @@ func activate(last_state : State) -> void:
 		"up":
 			sprite.offset = Vector2.ZERO
 			sprite.position = Vector2.ZERO
-			sprite.rotation_degrees = 90
-			
+			if sprite.flip_h:
+				sprite.rotation_degrees = 90
+			else:
+				sprite.rotation_degrees = -90
 		"down":
 			sprite.offset = Vector2.ZERO
 			sprite.position = Vector2.ZERO
-			sprite.rotation_degrees = -90
+			if sprite.flip_h:
+				sprite.rotation_degrees = -90
+			else:
+				sprite.rotation_degrees = 90
 			
 
 
